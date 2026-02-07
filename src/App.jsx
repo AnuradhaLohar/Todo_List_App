7cc9c18dc87facfeac5be8d59aa4d64fdbe95d6e
AnuradhaLohar <loharanuradha644@gmail.com>
2026-02-07 14:10:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-// import './App.css'
-// import { TodoList } from './component'
-
-// function App() {
-//   return (
-//     <div className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white min-h-screen flex items-center justify-center p-6">
-//       <div className="bg-gray-950 shadow-2xl rounded-2xl p-8 w-full max-w-2xl">
-//         <h1 className="text-3xl font-bold text-center mb-6 text-violet-400 tracking-wide">✨ My Todo List</h1>
-//         <TodoList />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
